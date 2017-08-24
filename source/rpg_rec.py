37fb592164ebd197bc47ba1f5771085e7f91203f
Darin LaSota <Darin.LaSota@gmail.com>
2017-08-24 14:07:50 -0700

    .

--- source/rpg_rec.py
+++ source/rpg_rec.py
@@ -5,7 +5,7 @@ from pyspark.mllib.recommendation import ALS
 
 # create spark session
 # use local[7] on ec2 instance
-spark = ps.sql.SparkSession.builder.master('local[7]').appName('rpgrec').getOrCreate()
+spark = ps.sql.SparkSession.builder.master('local[7]').appName('rpg_rec_ec2').getOrCreate()
 
 # load in the ratings data
 df = spark.read.csv('../data/ratings.csv',
